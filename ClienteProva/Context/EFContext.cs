dba212ef19eeaa5093da2c19b15f14e2bfbdf04e
Kevin-Souza <78280970+Kevin-Souza@users.noreply.github.com>
2021-10-04 23:19:54 -0300

    Create Delete

---
+++ ClienteProva/Context/EFContext.cs
@@ -0,0 +1,15 @@
+﻿using ClienteProva.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace ClienteProva.Context
+{
+    public class EFContext : DbContext
+    {
+        public EFContext() : base("CLIENTEPROVA") { }
+        public DbSet<Cliente> clientes { get; set; }
+    }
+}
---
+++ ClienteProva/Controllers/ClientesController.cs
@@ -0,0 +1,56 @@
+﻿using ClienteProva.Context;
+using ClienteProva.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ClienteProva.Controllers
+{
+    public class ClientesController : Controller
+    {
+        private EFContext context = new EFContext();
+
+        // GET: Clientes
+        public ActionResult Index()
+        {
+            return View(context.clientes.OrderBy(c => c.Nome));
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Cliente cliente)
+        {
+            context.clientes.Add(cliente);
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult Delete(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Cliente cliente = context.clientes.Find(id);
+            if (cliente == null)
+            {
+                return HttpNotFound();
+            }
+            return View(cliente);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(long id)
+        {
+            Cliente cliente = context.clientes.Find(id);
+            context.clientes.Remove(cliente);
+            context.SaveChanges();
+            return RedirectToAction("index");
+        }
+    }
+}
